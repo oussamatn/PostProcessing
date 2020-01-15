36f30e549c215bba9e25ff829c0ad6652e0a22fa
simyos <oussamajelliti@gmail.com>
2020-01-15 22:09:17 +0100

    Update to formidable with express

--- app.js
+++ app.js
@@ -1,53 +1,39 @@
 const express = require("express");
-const multer = require("multer");
 const cors = require("cors");
-
+const XLSX = require('xlsx'),  formidable = require('formidable');
+const bodyParser = require('body-parser');
 const app = express();
-app.use(cors());
-
-const fileFilter = (req, file, cb) => {
-  const allowedTypes = [
-    "application/xls",
-    "application/x-xls",
-    "image/jpeg",
-    "image/jpg",
-    "image/png"
-  ];
-  if (!allowedTypes.includes(file.mimetype)) {
-    const error = new Error("Incorrect file");
-    error.code = "INCORRECT_FILETYPE";
-    return cb(error, false);
-  }
-  cb(null, true);
-};
-
-const upload = multer({
-  dest: "./uploads",
-  fileFilter,
-  limits: {
-    fileSize: 50000000
-  }
-});
 
 const PORT = "8082" || process.env.PORT;
 
-app.post("/upload", upload.single("file"), (req, res) => {
-  console.log("uploading a file");
-  res.json({ file: req.file });
-});
+
+
+const FILE_PATH = 'uploads/';
+
+
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/public/index.html");
 });
 
-app.use((err, req, res, next) => {
-  if (err.code === "INCORRECT_FILETYPE") {
-    res.status(422).json({ error: "Only Excel are allowed" });
-    return;
-  }
-  if (err.code === "LIMIT_FILE_SIZE") {
-    res.status(422).json({ error: "Allow file size is 5MB" });
-    return;
-  }
-});
+app.post("/add", function(req, res) {
+    if (!file.originalname.match(/\.(xls|xlsx)$/)) {
+        return cb(new Error('Only Excel file are allowed.'), false);
+    }
+    const form = new formidable.IncomingForm();
+    form.parse(req, function(err, fields, files) {
+        const f = files[Object.keys(files)[0]];
+        const workbook = XLSX.readFile(f.path);
+        /* DO SOMETHING WITH workbook HERE */
+        const sheet = workbook.Sheets['Channel_'+1+'_1 '];
 
+        res.send({
+            status: true,
+            message: "workbook"
+        });
+    });
+});
+// add other middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
--- app_old.js
+++ app_old.js
@@ -1,42 +1,85 @@
-var express = require("express");
-var path = require("path");
-const bodyParser = require("body-parser");
+const express = require("express");
 const multer = require("multer");
-
-// SET STORAGE
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads')
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.fieldname + '-' + Date.now())
-  }
-})
-var upload = multer({ storage: storage })
-//CREATE EXPRESS APP
+const cors = require("cors");
+var XLSX = require('xlsx'),  formidable = require('formidable');
+const bodyParser = require('body-parser');
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
 
-//ROUTES WILL GO HERE
+const PORT = "8082" || process.env.PORT;
 
 
-app.get('/',function(req,res){
-  res.sendFile(__dirname + '/public/index.html');
- 
-});
 
- 
-
-app.post('/uploadfile', upload.single('myFile'), (req, res, next) => {
-  const file = req.file
-  if (!file) {
-    const error = new Error('Please upload a file')
-    error.httpStatusCode = 400
-    return next(error)
+const FILE_PATH = 'uploads/';
+const fileFilter = (req, file, cb) => {
+  // allow only "application/xls",
+  //     "application/x-xls",
+  if (!file.originalname.match(/\.(xls|xlsx)$/)) {
+    return cb(new Error('Only Excel file are allowed.'), false);
   }
-    res.send(file)
-  
-})
-var listener = app.listen(8080, function() {
-  console.log("Listening on port " + listener.address().port);
+  cb(null, true);
+};
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, FILE_PATH);
+  },
+  filename: (req, file, cb) => {
+    const fileName = file.originalname.toLowerCase().split(' ').join('-');
+    cb(null, fileName)
+  }
 });
+const upload = multer({
+  storage: storage,
+  fileFilter: fileFilter,
+  limits: {
+    fileSize: 5000000
+  }
+});
+
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/public/index.html");
+});
+app.post('/upload', upload.single('file'), async (req, res) => {
+  try {
+    const avatar = req.file;
+    // make sure file is available
+    if (!avatar) {
+      res.status(400).send({
+        status: false,
+        data: 'No file is selected.'
+      });
+    } else {
+      // send response
+      res.send({
+        status: true,
+        message: 'File is uploaded.',
+        data: {
+          name: avatar.originalname,
+          size: avatar.size
+        }
+      });
+
+    }
+
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+app.post("/add", function(req, res) {
+  const form = new formidable.IncomingForm();
+  form.parse(req, function(err, fields, files) {
+    const f = files[Object.keys(files)[0]];
+    const workbook = XLSX.readFile(f.path);
+    /* DO SOMETHING WITH workbook HERE */
+    console.log(workbook);
+    res.send({
+      status: true,
+      message: workbook
+    });
+  });
+});
+// add other middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
